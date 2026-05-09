cdf1e05edbbe4e73b996502532e066521185a30f
Josephleathem <Jleat2620@gmail.com>
2026-05-09 01:21:56 -0400

    Created home screen. Rebuilt app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.schoolpiggybankappassignment"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.schoolpiggybankappassignment"
